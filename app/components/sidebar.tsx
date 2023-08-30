765b68892bdae55cbb43345a6a1195d2aec94fdc
Monihon <monihon@qq.com>
2023-08-30 09:40:31 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          提高效率，改善人类生活。
+          加速创新，提高效率，改善人类生活。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
