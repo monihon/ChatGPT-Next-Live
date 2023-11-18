629fa9b21ced8a64c7e195f6d2563fc01f276052
Monihon <monihon@gmail.com>
2023-11-18 16:38:34 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,10 +145,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          轻松AI
+          EasyAI
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          轻松直播APP文案专用AI工具。
+          AI change the world.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
